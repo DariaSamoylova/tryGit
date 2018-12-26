1356c3d04b955ac14a802d752aabfa7536d09b17
DariaSamoylova <daria.borodina@gmail.com>
2018-12-26 17:16:36 +0300

    add

--- .idea/java/tryyy/TryClass.java
+++ .idea/java/tryyy/TryClass.java
@@ -1,4 +1,8 @@
 package tryyy;
 
 public class TryClass {
+    public static void main(String[] args) {
+        System.out.println("123456");
+        System.out.println("123456");System.out.println("123456");System.out.println("123456");
+    }
 }
